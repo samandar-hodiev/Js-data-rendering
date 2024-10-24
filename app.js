3408c5a8fc615c081bf8bcb29b4f74261c7593f1
samandar-hodiev <samandarkhodiev04@gmail.com>
2024-10-25 04:03:48 +0500

    I used <template></template> rendering datas

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ userDatas.forEach((el) => {
 
     const introElement = document.createElement('div');
 
-    introElement.classList.add('w-[320px]', 'h-[640px]', 'p-2', 'bg-green-100', 'rounded','mt-3','hover:shadow-2xl','cursor-pointer');
+    introElement.classList.add('w-[320px]', 'h-[640px]', 'p-2', 'bg-green-100', 'rounded', 'mt-3', 'hover:shadow-2xl', 'cursor-pointer');
     introElement.innerHTML = `
      <img src="${el.img}" class="rounded">
-     <h1 class="text-xl text-blue-800">${el.fName} ${ el.lName}</h1>
+     <h1 class="text-xl text-blue-800">${el.fName} ${el.lName}</h1>
     <p class="text-gray-500">
         ${el.info}
     </p>
@@ -39,6 +39,108 @@ userDatas.forEach((el) => {
 `;
     intro.append(introElement);
 });
+
+
+
+
+
+
+
+
+
+// ------------------------------ data maping (creating with function) ------------------------------ //
+const renderPlace = $("#mainn");
+console.log(renderPlace);
+
+
+// userDatas.forEach((element)=>{
+
+//     const meinElement = ('div');
+
+//     meinElement.classList.add();
+
+//     meinElement.innerHTML=`
+    
+//       <img src="${element.img}">
+
+//       <h2 class="text-black text-[red]">
+//        ${element.fName} ${ element.lName}
+//       </h2>
+
+//       <p class="text-gray-400">
+//          ${element.info}
+//       </p>
+
+//       <p class="text-blue-500">
+//          ${element.email}
+//       </p>
+      
+//       <span class="text-green-600">
+//          ${element.id}
+//       </span>
+//     `;
+
+//     renderPlace.append(meinElement);
+// });
+
+
+userDatas.forEach((e)=>{
+    const box = creatorFunc('div','w-[320px] h-[640px] p-2 bg-gray-300 rounded mt-4',`
+    
+      <img src="${e.img}">
+
+      <h2 class="text-black text-[red]">
+       ${e.fName} ${ e.lName}
+      </h2>
+
+      <p class="text-gray-700">
+         ${e.info}
+      </p>
+
+      <p class="text-blue-500">
+         ${e.email}
+      </p>
+      
+      <span class="text-green-600">
+         ${e.id}
+      </span>
+    `);
+
+    renderPlace.append(box);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------ data rendering with  <template> </template> ------------------------------ //
+const enderSection = $("#end");
+// console.log(enderSection);
+
+const temp = $(".content");
+// console.log(temp);
+
+const cloneTemp = temp.content.cloneNode(true);
+// console.log(cloneTemp);
+
+const renderContent = $('#end');
+// console.log(renderContent);
+
+renderContent.append(cloneTemp);
+
+
+
+
+
+
+
 
 
 
@@ -65,7 +167,7 @@ const link = $$("#menu li a");
 // console.log(link);
 
 
-language.addEventListener('change',(evt)=>{
+language.addEventListener('change', (evt) => {
 
     // console.log(evt.target.value);
     // console.log(lang[evt.target.value]);
@@ -84,16 +186,16 @@ language.addEventListener('change',(evt)=>{
 });
 
 
-function seLang(){
+function seLang() {
     const local = JSON.parse(localStorage.getItem('lang'));
 
-    link[0].textContent=local.home;
-    link[1].textContent=local.contact;
-    link[2].textContent=local.about;
-    link[3].textContent=local.portofilo;
-    link[4].textContent=local.usage;
+    link[0].textContent = local.home;
+    link[1].textContent = local.contact;
+    link[2].textContent = local.about;
+    link[3].textContent = local.portofilo;
+    link[4].textContent = local.usage;
 
-    seLang();
+
 };
 
 seLang();
--- utils.js
+++ utils.js
@@ -1,10 +1,26 @@
 "use strict";
 
-
+// ------------ selector function ------------ //
 function $(selector){
     return document.querySelector(selector);
 };
 
 function $$(selector){
     return document.querySelectorAll(selector);
+};
+
+
+
+
+
+
+
+
+// ------------ function of createElement ------------ //
+const creatorFunc = (tags, classes, contents)=>{
+    const elements = document.createElement(tags);
+          elements.setAttribute('class',classes);
+          elements.innerHTML=contents;
+
+    return elements;
 };
